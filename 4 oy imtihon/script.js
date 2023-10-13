b6cd055db83c9a5bafaf12bab9c3a7a6fb49a0a9
husaN2tru <146550074+husaN2tru@users.noreply.github.com>
2023-10-13 22:53:48 +0500

    Add files via upload

---
+++ 4 oy imtihon/script.js
@@ -0,0 +1,8 @@
+function showText() {
+    document.querySelector('.hide-text').classList.add('show')
+}
+
+
+function hideText() {
+    document.querySelector('.hide-text ').classList.remove('show')
+}
